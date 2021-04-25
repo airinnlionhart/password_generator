406fb941406d8ae047566697eab148d91ab749a9
alenharta <alenharta@alenhartas-air.myfiosgateway.com>
2021-04-25 13:56:38 -0400

    added a template

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(requests):
-    return HttpResponse("hello friend")
+def home(request):
+    return render(request, 'generator/home.html', {'password':"testingthis"})
